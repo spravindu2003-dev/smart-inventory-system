365ae336b751d3b4d1bc201ccf7fa310b50cd63d
SP Ravindu <spravindu2003@gmail.com>
2026-04-30 22:01:23 +0530

    Initial React frontend setup

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+        <div>
+
+          <header>
+          <h1>Smart Inventory System</h1>
+          </header>
+          
+          <main>
+          <h2>Dashboard</h2>
+          </main>
+          
+        </div>
+        
+  )
+}
+
+export default App
